81ef8ae1854d89edfcdec0aab9c082e7c2aa2482
ignacioRosales <nachorosales2604@gmail.com>
2023-10-03 13:56:27 -0300

    Escribi los datos y cree el main

---
+++ main.rb
@@ -0,0 +1,16 @@
+datos = [ {name => 'John', 
+    dni => 2314151, 
+    apellido => 'Salchichon',
+    provincia => "santa fe",
+    ciudad => "San lorenzo",
+    calle => "San martin",
+    altura => 123},
+    
+  {name => 'Arco', 
+    dni => 1245124, 
+    apellido => 'Salomon',
+    provincia => "santa fe",
+    ciudad => "Rosario",
+    calle => "Ubunto",
+    altura => 1234}
+]

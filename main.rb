e147b4a2be4c17694987af024e7a559abec6a10b
ignacioRosales <nachorosales2604@gmail.com>
2023-10-03 14:20:23 -0300

    creacion de clase persona y creacion metodo agregar mascota

--- main.rb
+++ main.rb
@@ -1,16 +1,43 @@
-datos = [ {name => 'John', 
-    dni => 2314151, 
-    apellido => 'Salchichon',
-    provincia => "santa fe",
-    ciudad => "San lorenzo",
-    calle => "San martin",
-    altura => 123},
-    
-  {name => 'Arco', 
-    dni => 1245124, 
-    apellido => 'Salomon',
-    provincia => "santa fe",
-    ciudad => "Rosario",
-    calle => "Ubunto",
-    altura => 1234}
+datos = [ 
+    {
+        name => "John", 
+        dni => 2314151, 
+        apellido => "Salchichon",
+        provincia => "santa fe",
+        ciudad => "San lorenzo",
+        calle => "San martin",
+        altura => 123
+    },
+
+    {
+        name => "Arco", 
+        dni => 1245124, 
+        apellido => 'Salomon',
+        provincia => "santa fe",
+        ciudad => "Rosario",
+        calle => "Ubunto",
+        altura => 1234
+    }
 ]
+
+
+class Persona
+    attr_accessor :nombre, :dni, :apellido, :provinica, :ciudad, :calle, :altura
+    
+    def initialize(nombre, dni, apellido, provincia, ciudad, calle, altura)
+        @nombre = nombre
+        @dni = dni
+        @apellido = apellido
+        @provincia = provincia
+        @ciudad = ciudad
+        @calle = calle
+        @altura = altura
+    end
+
+    def agregarMascota(nombre, raza)
+        @nombreMascota = nombre
+        @raza = raza
+    end
+
+
+end

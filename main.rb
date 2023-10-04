edbee4bbef1236a2f98d1488bec1b65f4976346a
unknown <tomasperesin@gmail.com>
2023-10-04 14:42:33 -0300

    agregue una forma para escribir el archivo

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ datos = [ {:nombre => 'John',
 ]
 
 class Persona
-    attr_accessor :nombre, :dni, :apellido, :provinica, :ciudad, :calle, :altura
+    attr_accessor :nombre, :dni, :apellido, :provincia, :ciudad, :calle, :altura, :nombreMascota, :raza
     
     def initialize(nombre, dni, apellido, provincia, ciudad, calle, altura)
         @nombre = nombre
@@ -28,12 +28,16 @@ class Persona
         @altura = altura
     end
 
-    def inspect()
-        p "estas es un inspect"
-    end
-
-    def to_s()
-        p "to_string estas en"
+    def to_s
+            "nombre: #{@nombre}" + ", " +
+            "dni: #{@dni}" + ", " +
+            "apellido: #{@apellido}" + ", " +
+            "provincia: #{@provincia}" + ", " +
+            "ciudad: #{@ciudad}" + ", " +
+            "calle: #{@calle}" + ", " +
+            "altura: #{@altura}" + ", " +
+            "mascota: #{@nombreMascota}" + ", " +
+            "raza: #{@raza}"
     end
 
     def agregarMascota(nombre, raza)
@@ -58,8 +62,6 @@ mascotas.push(Mascota.new("Junior", "Labrador"))
 mascotas.push(Mascota.new("Cobe","Border collie")) 
 mascotas.push(Mascota.new("Ringo","Caniche")) 
 mascotas.push(Mascota.new("Hercules", "Yorkshire"))
-mascotas.push(Mascota.new("India", "Pastor aleman"))
-mascotas.push(Mascota.new("Colo", "pitbull"))
 
 def crearPersona(a)
     per = Array.new
@@ -72,7 +74,7 @@ end
 
 def asignaMascota(array, mascota)
     array.each do |persona|
-        i = rand(7)
+        i = rand(5)
         persona.agregarMascota(mascota[i].nombre, mascota[i].raza)
     end
     array
@@ -84,16 +86,6 @@ personas = asignaMascota(personas, mascotas)
 file = File.open("personas.txt", 'w')
 personas.each do |persona|
     file.write(persona)
-    file.write(" ")
+    file.write("\n")
 end
-
-personas.each_with_index do |persona, i|
-    p i
-    p persona
-    puts persona
-end
-
-# file.write("personas.txt", personas)
-# file.close
-
-# puts Object.methods
+file.close
